631e66efade5c95162cb07ca6af90e7058d5aea5
Ameya <ameyaajay.joshi@sjsu.edu>
2015-02-22 21:24:07 -0800

    Adding fields to domain..

--- src/main/java/com/glassify/adserver/domain/Ad.java
+++ src/main/java/com/glassify/adserver/domain/Ad.java
@@ -1,10 +1,19 @@
 package com.glassify.adserver.domain;
 
+import java.sql.Date;
+
 public class Ad {
 
 	private int id;
 	private String name;
-	private String role;
+	private String type;
+	private String url;
+	private String company;
+	private String location;
+	private String language;
+	private String content;
+	private Date createdDate;
+	private Date expiryDate;
 	
 	public int getId() {
 		return id;
@@ -18,10 +27,53 @@ public class Ad {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getRole() {
-		return role;
+	public String getType() {
+		return type;
 	}
-	public void setRole(String role) {
-		this.role = role;
+	public void setType(String type) {
+		this.type = type;
 	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	
 }
